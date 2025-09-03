bbd7d44cfdc72f3332a234f7280c77b8fb62c380
Bimaahmadani <bimaahmadani@gmail.com>
2025-09-04 00:08:46 +0700

    first commit

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,10 @@
+import '../css/Favorites.css'
+
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>Your favorite list is empty</h2>
+        <p>Add some movies to your favorite list</p>
+    </div>
+}
+
+export default Favorites
